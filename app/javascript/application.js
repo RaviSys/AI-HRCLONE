89d3c76101a0d69dcc6695d2057076b56f71b000
Ravi Singh <ravi@appsimpact.com>
2022-02-11 12:41:39 +0530

    working with rails ujs and bootstrap bundle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
+import Rails from '@rails/ujs'
+
+Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.2/lib/assets/compiled/rails-ujs.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
