49a442eea6bf270b2b2ace802dc0e5dbf19765ad
Ravi Singh <ravi@appsimpact.com>
2022-02-12 14:13:51 +0530

    added bs5 alert messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when :success then "alert alert-success"
+      when :notice then "alert alert-success"
+      when :alert then "alert alert-danger"
+      when :error then "alert alert-danger"
+      when :warning then "alert alert-warning"
+    end
+  end
 end
